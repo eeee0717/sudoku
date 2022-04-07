b9aa2f5c85eebd3bcb7d98a497332bb92ddda182
eeee0717 <761050293@qq.com>
2022-04-07 22:28:52 +0800

    界面完成

--- sudoku/MainWindow.xaml.cs
+++ sudoku/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace sudoku
 {
@@ -23,6 +10,22 @@ namespace sudoku
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Generate_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Revoke_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Solve_Click(object sender, RoutedEventArgs e)
+        {
         }
     }
 }
